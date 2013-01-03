1634a69c5cd5a54e619f87a2cbdd635e5983f47f
Timon Tschanz <timon.tschanz@4teamwork.ch>
2013-01-03 18:25:12 +0100

    fix tests

--- ftw/statusmap/tests/test_transition_execution.py
+++ ftw/statusmap/tests/test_transition_execution.py
@@ -1,29 +1,27 @@
 from ftw.statusmap.testing import FTW_STATUSMAP_INTEGRATION_TESTING
 from unittest2 import TestCase
 from ftw.statusmap.utils import executeTransition
-
+from Products.CMFCore.utils import getToolByName
 
 class TestTransitionExecution(TestCase):
 
     layer = FTW_STATUSMAP_INTEGRATION_TESTING
 
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.wf_tool = getToolByName(self.portal, 'portal_workflow')
+        self.wf_tool.setDefaultChain('simple_publication_workflow')
+        self.cat = getToolByName(self.portal, 'portal_catalog')
 
     def test_execution(self):
-        portal = self.layer['portal']
-        portal.invokeFactory('Document', 'brain1')
-        brain1 = portal['brain1']
+        self.portal.invokeFactory('Document', 'brain1')
+        brain1 = self.portal['brain1']
         brain1.invokeFactory('Document', 'brain2')
-        dicts = [
-            {'path':'/Plone/brain1',
-             'review_state':'private',
-             'type': 'Document',
-             'workflow': 'simple_publication_workflow',
-             },
-            {'path':'/Plone/brain1/brain2',
-             'review_state':'published',
-             'type': 'Document',
-             'workflow': 'simple_publication_workflow',
-             }
+        brain2 = brain1['brain2']
+
+        uids = [
+            brain1.UID(),
+            brain2.UID()
             ]
-        executeTransition(portal.portal_workflow, 'publish', dicts)
-        self.assertEqual(len(portal.portal_catalog.searchResults({'review_state':'published'})), 2)
+        executeTransition(self.portal, self.wf_tool, 'publish', uids, 'Lorem')
+        self.assertEqual(len(self.cat.searchResults({'review_state':'published'})), 2)
--- ftw/statusmap/tests/test_view_functional.py
+++ ftw/statusmap/tests/test_view_functional.py
@@ -1,16 +1,16 @@
-from ftw.statusmap.testing import FTW_STATUSMAP_INTEGRATION_TESTING
+from ftw.statusmap.testing import FTW_STATUSMAP_FUNCTIONAL_TESTING
 from unittest2 import TestCase
 from Products.CMFCore.utils import getToolByName
 from plone.testing.z2 import Browser
 import transaction
 from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
 
-class TestStatusmapView(TestCase):
+class TestStatusmapViewFunctional(TestCase):
 
-    layer = FTW_STATUSMAP_INTEGRATION_TESTING
+    layer = FTW_STATUSMAP_FUNCTIONAL_TESTING
 
     def setUp(self):
-        super(TestStatusmapView, self).setUp()
+        super(TestStatusmapViewFunctional, self).setUp()
         self.portal = self.layer['portal']
         self.wf_tool = getToolByName(self.portal, 'portal_workflow')
         self.wf_tool.setDefaultChain('simple_publication_workflow')
@@ -18,11 +18,12 @@ class TestStatusmapView(TestCase):
 
         doc1 = self.portal.get(self.portal.invokeFactory('Folder', 'folder1'))
         self.portal.invokeFactory('Document', 'document2')
-        doc1.invokeFactory('Document', 'document3')
+        self.doc2 = doc1.get(doc1.invokeFactory('Document', 'document3'))
         transaction.commit()
 
     def test_view_browser(self):
         browser = Browser(self.layer['app'])
+        browser.handleErrors = False
         browser.addHeader('Authorization', 'Basic %s:%s' % (TEST_USER_NAME, TEST_USER_PASSWORD,))
         browser.open(self.portal.absolute_url()+'/statusmap')
         self.assertIn('<a href="http://nohost/plone/folder1/document3"', browser.contents)
@@ -31,5 +32,16 @@ class TestStatusmapView(TestCase):
         self.assertIn('<label for="publish">Publish</label>', browser.contents)
         self.assertIn('<label for="submit">Submit for publication</label>', browser.contents)
 
-        browser.post('statusmap', data="form.submitted=1&uids='445i85-556986-55969'")
-        import pdb; pdb.set_trace()
+        browser.post('statusmap', data="form.submitted=1&uids:list=445i85-556986-55969")
+        self.assertIn('Please select a Transition', browser.contents)
+
+        browser.post('statusmap', data="form.submitted=1&transition=publish")
+        self.assertIn('Please select at least one Item', browser.contents)
+
+        browser.post('statusmap', data="form.submitted=1")
+        self.assertIn('Please select at least one Item', browser.contents)
+        self.assertIn('Please select a Transition', browser.contents)
+
+        data = "form.submitted=1&uids:list=%s&transition=publish" % self.doc2.UID()
+        browser.post('statusmap', data=data)
+        self.assertIn('Transition executed successfully.', browser.contents)
